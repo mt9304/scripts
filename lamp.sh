c01966277f54757a91c601fb0e2ed86d729452c0
Max <mt9304@gmail.com>
2017-12-27 22:17:17 -0800

    Uncommented some things to try again next time.

--- lamp.sh
+++ lamp.sh
@@ -2,24 +2,28 @@
 
 #Remember to execute with bash script.sh instead of sh script.sh because default system shell is dash, not bash, and dash does not have the triple angle brackets. 
 
-#yes | sudo apt-get update
-#yes | sudo apt-get install apache2
-#echo "Finished installing Apache2. Restarting Apache server. "
-#sudo systemctl restart apache2
-#sudo ufw allow in "Apache Full"
+yes | sudo apt-get update
+yes | sudo apt-get install apache2
+echo "Finished installing Apache2. Restarting Apache server. "
+sudo systemctl restart apache2
+sudo ufw allow in "Apache Full"
 
 #Automates the asking for password part when installing MySQL
 #Remember to remove password here and try to change it in the sql script later. 
 #sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password change_this'
 #sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password change_this'
 
-#echo "Installing MySQL Server. "
-#sudo apt-get -y install mysql-server
-#echo "Finished installing MySQL Server. "
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password'
+
+echo "Installing MySQL Server. "
+sudo apt-get -y install mysql-server
+echo "Finished installing MySQL Server. "
 
 echo "Running the necessary queries to simulate mysql_secure_installation. "
 # Make sure that NOBODY can access the server without a password
-##mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
 # Kill the anonymous users
 mysql -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
