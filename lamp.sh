04613229fd83a2edb20e31b54aba004070fe33bb
mt9304@gmail.com <mt9304@gmail.com>
2019-01-13 20:45:23 -0800

    Fixing lamp wp-config.php file in script.

--- lamp.sh
+++ lamp.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#Remember to execute with bash script.sh instead of sh script.sh because default system shell is dash, not bash, and dash does not have the triple angle brackets. 
+#Remember to execute with bash script.sh instead of sh script.sh because default system shell is dash, not bash, and dash does not have the triple angle brackets.
 
-#echo "Default root password for mysql uses 'CHANGEME', remember to change this if needed. "
+#echo "Default root password for mysql uses '${ROOTDBPASS}', remember to change this if needed. "
 echo "Enter your website's name. This will also be used as the database name, and the database/website user will be this name plus '_user'. "
 read SITENAME
 echo "Enter your website's database and admin password. You can change them later. "
 read SITEPASS
 
 SITEUSER=$SITENAME"_user"
+ROOTDBPASS="CHANGEME"
 
 yes | sudo apt install vim
 yes | sudo apt install curl
@@ -24,13 +25,13 @@ sudo systemctl restart apache2
 sudo ufw allow in "Apache Full"
 
 #Automates the asking for password part when installing MySQL
-#Remember to remove password here and try to change it in the sql script later. 
+#Remember to remove password here and try to change it in the sql script later.
 #sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password change_this'
 #sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password change_this'
 
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password CHANGEME'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password CHANGEME'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password ${ROOTDBPASS}'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ${ROOTDBPASS}'
 
 echo "Installing MySQL Server. "
 sudo apt-get -y install mysql-server
@@ -39,20 +40,20 @@ echo "Finished installing MySQL Server. "
 #Taken from https://stackoverflow.com/questions/24270733/automate-mysql-secure-installation-with-echo-command-via-a-shell-script/35004940
 echo "Running the necessary queries to simulate mysql_secure_installation. "
 # Make sure that NOBODY can access the server without a password
-mysql --user="root" --password="CHANGEME" -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+mysql --user="root" --password="${ROOTDBPASS}" -e "UPDATE mysql.user SET Password = PASSWORD('${ROOTDBPASS}') WHERE User = 'root'"
 # Kill the anonymous users
-mysql --user="root" --password="CHANGEME" -e "DROP USER ''@'localhost'"
+mysql --user="root" --password="${ROOTDBPASS}" -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here
-mysql --user="root" --password="CHANGEME" -e "DROP USER ''@'$(hostname)'"
+mysql --user="root" --password="${ROOTDBPASS}" -e "DROP USER ''@'$(hostname)'"
 # Kill off the demo database
-mysql --user="root" --password="CHANGEME" -e "DROP DATABASE test"
+mysql --user="root" --password="${ROOTDBPASS}" -e "DROP DATABASE test"
 # Use password for connecting to database
 #ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-mysql --user="root" --password="CHANGEME" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'CHANGEME';"
+mysql --user="root" --password="${ROOTDBPASS}" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${ROOTDBPASS}';"
 # Make our changes take effect
-mysql --user="root" --password="CHANGEME" -e "FLUSH PRIVILEGES"
+mysql --user="root" --password="${ROOTDBPASS}" -e "FLUSH PRIVILEGES"
 # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'CHANGEME';"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${ROOTDBPASS}';"
 echo "Finished running mysql_secure_installation through manual queries. "
 
 #Installing PHP
@@ -61,7 +62,7 @@ yes | sudo apt install php libapache2-mod-php php-mysql
 #Make Apache prioritize PHP files over html
 sudo rm /etc/apache2/mods-enabled/dir.conf
 cat << EOF | sudo tee /etc/apache2/mods-enabled/dir.conf
-#Installing 
+#Installing
 <IfModule mod_dir.c>
         DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
 </IfModule>
@@ -76,9 +77,9 @@ fi
 ##############################################################################################################################
 
 #Wordpress database setup
-mysql --user="root" --password="CHANGEME" -e "CREATE DATABASE ${SITENAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-mysql --user="root" --password="CHANGEME" -e "GRANT ALL ON ${SITENAME}.* TO ${SITEUSER}@'localhost' IDENTIFIED BY '${SITEPASS}';"
-mysql --user="root" --password="CHANGEME" -e "FLUSH PRIVILEGES;"
+mysql --user="root" --password="${ROOTDBPASS}" -e "CREATE DATABASE ${SITENAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+mysql --user="root" --password="${ROOTDBPASS}" -e "GRANT ALL ON ${SITENAME}.* TO ${SITEUSER}@'localhost' IDENTIFIED BY '${SITEPASS}';"
+mysql --user="root" --password="${ROOTDBPASS}" -e "FLUSH PRIVILEGES;"
 
 sudo apt update
 yes | sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
@@ -99,7 +100,7 @@ mkdir /tmp/wordpress/wp-content/upgrade
 
 WPSALT="$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)"
 
-cat << EOF | sudo tee /tmp/$SITENAME/wp-config.php
+cat << EOF | sudo tee /tmp/wordpress/wp-config.php
 <?php
 /** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
@@ -168,7 +169,7 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-                                          
+
 EOF
 
 sudo cp -a /tmp/wordpress/. /var/www/$SITENAME
@@ -177,6 +178,6 @@ sudo find /var/www/$SITENAME/ -type d -exec chmod 750 {} \;
 sudo find /var/www/$SITENAME/ -type f -exec chmod 640 {} \;
 
 
-echo "LAMP with Wordpress site installed. Default mysql root password is 'CHANGEME', remember to change this if needed. "
+echo "LAMP with Wordpress site installed. Default mysql root password is '${ROOTDBPASS}', remember to change this if needed. "
 echo "Remember to change Document root in config file: sudo vi /etc/apache2/sites-available/000-default.conf"
 echo "Then restart the web server: sudo systemctl restart apache2"
