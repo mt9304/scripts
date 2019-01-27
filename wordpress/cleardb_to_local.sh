0129a5a4efe42e654359be9deb1c265d2381d730
mt9304@gmai.com <mt9304@gmai.com>
2019-01-26 17:30:19 -0800

    Fixed typo.

--- wordpress/cleardb_to_local.sh
+++ wordpress/cleardb_to_local.sh
@@ -2,7 +2,7 @@
 #Then changes the siteurl and home columns in the wp_potions table to http://localhost
 #Then restores your local MySQL database with this .sql file
 
-#TO get host, run herok config --app appname to get the part that looks like us-fewe-wood-south-20.cleardb.net
+#To get host, run herok config --app appname to get the part that looks like us-fewe-wood-south-20.cleardb.net
 #Rest can be found in Heroku > app > cleardb > database > stem information
 mysqldump --host=changeme --user=changeme --password=changeme heroku_changeme > appname-$(date +%F)-cleardb-to-local.sql
 sed -i -e "s/'siteurl','http:\/\/mediocreinventions.herokuapp.com\/'/'siteurl','http:\/\/localhost'/g" appname-$(date +%F)-cleardb-to-local.sql
