a59de6d8fa67010e1d943144eb03770ad358087c
mt9304 <mt9304@gmail.com>
2019-02-26 11:30:36 -0800

    Added headers.

--- generate-test-files/generate.sh
+++ generate-test-files/generate.sh
@@ -9,10 +9,13 @@
 #mkdir custdata/WFF_b2b/{20180101,20180102,20180103,20180104,20180105,20180106,20180107,20180108,20180109,20180110};
 #mkdir esldata/WFF_b2b/{20180101,20180102,20180103,20180104,20180105,20180106,20180107,20180108,20180109,20180110};
 
+# Initialize current directories
+current_dir=$('pwd')
+
 # Arrays of useful words
 list_of_names=(
     Olivia Ruby Emily Grace Jessica Chloe Sophie Lily Amelia Evie
-    Mia Ella Jack Oliver Thomas Harry Jishua Alfie Charlie Daniel
+    Mia Ella Jack Oliver Thomas Harry Joshua Alfie Charlie Daniel
     Table Chair Cup Spoon Pad Tree Mouse Mango Banana Ginger
     );
 # echo ${list_of_names[9]}; to echo an index
@@ -29,10 +32,16 @@ list_of_ethnicities=(
     Caucasian Asian EastAsian WestAsian NorthAsian SouthAsian NorthEastAsian NorthWestAsian SouthEastAsian SouthWestAsian
     );
 
-for i in {0..9}
+# Create the file and add headers
+cat <<EOF > test.txt
+FiscalMonth|EmployeeID|LastName|FirstName|JobTitle|Department|BaseSalary|Location0|Ethnicity
+EOF
+
+for i in {51243..51542}
 do
-# Date | Last Name                            | First Name                           | Job Title                                | Department                                | BaseSalary                | Location0                               | Ethnicity
+# Date | EID| Last Name                            | First Name                           | Job Title                                | Department                                | BaseSalary                | Location0                               | Ethnicity
 cat <<EOF >> test.txt
-2018-01|${list_of_names[$((0 + RANDOM % 29))]}|${list_of_names[$((0 + RANDOM % 29))]}|${list_of_job_titles[$((0 + RANDOM % 9))]}|${list_of_departments[$((0 + RANDOM % 4))]}|$((48000 + RANDOM % 85000))|${list_of_countries[$((0 + RANDOM % 4))]}|${list_of_ethnicities[$((0 + RANDOM % 9))]}
+2018-01|${i}|${list_of_names[$((0 + RANDOM % 29))]}|${list_of_names[$((0 + RANDOM % 29))]}|${list_of_job_titles[$((0 + RANDOM % 9))]}|${list_of_departments[$((0 + RANDOM % 4))]}|$((48000 + RANDOM % 85000))|${list_of_countries[$((0 + RANDOM % 4))]}|${list_of_ethnicities[$((0 + RANDOM % 9))]}
 EOF
 done
+
